7308314a90501a7c5d6b535d483d326c451c439a
Quarantus <quarantus@yahoo.com>
2026-04-10 17:56:51 +0800

    Update analyze.js

--- functions/api/analyze.js
+++ functions/api/analyze.js
@@ -1,12 +1,21 @@
 import { parseHTML } from 'linkedom';
 
-export const config = { api: { bodyParser: false } };
+export const config = {
+  api: { bodyParser: false },
+};
 
 export default async function handler(req, res) {
-  if (req.method!== 'PUT') return res.status(405).end();
+  if (req.method!== 'PUT') return res.status(405).end('Method Not Allowed');
+
   const buffers = [];
   for await (const chunk of req) buffers.push(chunk);
-  const html = Buffer.concat(buffers).toString('utf8').split('name="file"')[1].split('\r\n\r\n')[1].split('\r\n----')[0];
+  const body = Buffer.concat(buffers).toString('utf8');
+  const boundary = body.split('\r\n')[0];
+  const parts = body.split(boundary);
+  const filePart = parts.find(p => p.includes('name="file"'));
+  if (!filePart) return res.status(400).send('No file');
+
+  const html = filePart.split('\r\n\r\n')[1].split('\r\n--')[0];
 
   const { document } = parseHTML(html);
   const blocks = [];
@@ -20,5 +29,5 @@ export default async function handler(req, res) {
       });
     }
   });
-  res.json({ blocks });
+  res.status(200).json({ blocks });
 }
